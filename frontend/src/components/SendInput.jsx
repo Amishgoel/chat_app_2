23f3191682eefd272ec26d1575505ff24b1f3a58
Amishgoel <amish.goel.ug23@nsut.ac.in>
2025-07-16 21:59:38 +0530

    Update SendInput.jsx

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -13,7 +13,7 @@ const SendInput = () => {
 
         e.preventDefault();
         try{
-           const res= await axios.post(`http://localhost:8080/api/v1/message/send/${selectedUser?._id}`,{message},{
+           const res= await axios.post(`https://chat-app-backend-cod3.onrender.com/api/v1/message/send/${selectedUser?._id}`,{message},{
             headers:{
                 'Content-Type': 'application/json',
 
